f88a156cfcf96969c0e2ffcd8b1ad4ee136a4792
John Smith <cat@gmail.com>
2019-04-21 17:29:23 -0500

    Fixed AOP config. Added new example AOP aspect

--- src/main/java/com/naturalprogrammer/spring5tutorial/aop/RequestMonitor.java
+++ src/main/java/com/naturalprogrammer/spring5tutorial/aop/RequestMonitor.java
@@ -7,7 +7,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
-@Component
 @Aspect
 public class RequestMonitor {
 	
@@ -15,13 +14,12 @@ public class RequestMonitor {
 
 	@Around("within(com.naturalprogrammer.spring5tutorial.controllers.*)")
 	public Object wrap(ProceedingJoinPoint pjp) throws Throwable {
-		
-		log.info("Before handler " + pjp.getSignature().getName()
+		log.debug("Before handler " + pjp.getSignature().getName()
 				+ ". Thread " + Thread.currentThread().getName());
 		
 		Object retval = pjp.proceed();
 		
-		log.info("Handler " + pjp.getSignature().getName()
+		log.debug("Handler " + pjp.getSignature().getName()
 				+ " execution successful.");
 		
 		return retval;
